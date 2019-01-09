2f1476fd1989f1d72422069523bd928325de8efb
fkccoding <840880472@qq.com>
2019-01-09 11:29:57 +0800

    Initial commit

---
+++ src/main/java/top/chuckfang/gittest/entity/Log.java
@@ -0,0 +1,12 @@
+package top.chuckfang.gittest.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: www.chuckfang.top
+ * @Date: 2019/1/9 11:26
+ */
+@Data
+public class Log {
+    private Long id;
+}
